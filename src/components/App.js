27df05b67ead4f90d599a44709e214cd4a198f9f
Stean <stean.ngugi@student.moringaschool.com>
2025-04-16 23:53:11 +0300

    Final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect} from 'react';
+
+function App() {
+    const [dogImage, setDogImage] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+            setDogImage(data.message);
+            setIsLoading(false);
+        });
+    }, []);
+    return (
+        <div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <img src={dogImage} alt="A Random Dog " />
+            )}
+        </div>
+    );
+}
+export default App;
